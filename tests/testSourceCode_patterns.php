d312fba0f6039318f027ccb2177b7d054c94583e
megaplanet <megaplanet.github@megaplanet.org>
2012-05-18 02:43:10 +0200

    change again the way to compute directory

--- tests/testSourceCode_patterns.php
+++ tests/testSourceCode_patterns.php
@@ -5,8 +5,13 @@ require_once '../SourceCode.php' ;
 require_once '../CSV.php' ;
 define('OUTPUT_DIR','data/generated/sourceCodePatterns') ;
 define('RULES_FILE','data/input/FileSystemPattern.csv') ;
-define('EXPLORE_DIR',ABSPATH_BASE.'101results/101repo/contributions') ;  // /gwtTree
-echo '<h2>Exploring and matching the directory '.EXPLORE_DIR.'</h2>' ;
+$root = addToPath(ABSPATH_BASE,'101results') ;
+if (!is_dir($root)) {
+  $root = addToPath(ABSPATH_BASE,'../101results') ;
+}
+$exploreDir = addToPath($root,'101repo/contributions') ;  // /gwtTree
+
+echo '<h2>Exploring and matching the directory '.$exploreDir.'</h2>' ;
 $matcher = new FileSystemPatternMatcher(RULES_FILE) ;
 $groups=array(
     'languages' => 
@@ -20,5 +25,5 @@ $groups=array(
            'groupedBy' => 'technology'
        )
  ) ;
-$r = $matcher->generate(EXPLORE_DIR,OUTPUT_DIR,$groups) ;
+$r = $matcher->generate($exploreDir,OUTPUT_DIR,$groups) ;
 echo 'files generated are in <a href="'.OUTPUT_DIR.'" target="_blank">'.OUTPUT_DIR.'</a>' ;
